7e0ae5b79dc40d1c487b28e37d01e8ef7dc7afc5
Eric Kuzmenko <eric.n.kuzmenko@gmail.com>
2022-10-23 00:00:00 +0000

    Add forward, back, left, right, yaw, and pitch inputs which are exposed as DataElements on the component so they have text fields that can define each key within the editor
    Use the StartingPointInput gem and its InputEventNotificationBus to
    track when an input is pressed, released, or held.
    
    Each of the input names are stored in separate AZStd::string variables
    because exposing the StartingPointInput::InputNotificationId variables directly via the DataElement call does not present a text field inside the editor. So instead, strings are used and then the Input IDs are assigned to their values using those strings. The keys are assigned when the entity is activated.

--- Code/Source/Clients/FirstPersonControllerComponent.cpp
+++ Code/Source/Clients/FirstPersonControllerComponent.cpp
@@ -1,13 +1,23 @@
+#include <AzCore/Component/TickBus.h>
 #include <Clients/FirstPersonControllerComponent.h>
 #include <AzCore/Serialization/EditContext.h>
+#include <StartingPointInput/InputEventNotificationBus.h>
 
 namespace FirstPersonController
 {
+    using namespace StartingPointInput;
+
     void FirstPersonControllerComponent::Reflect(AZ::ReflectContext* rc)
     {
         if(auto sc = azrtti_cast<AZ::SerializeContext*>(rc))
         {
             sc->Class<FirstPersonControllerComponent, AZ::Component>()
+              ->Field("Forward Key", &FirstPersonControllerComponent::m_str_Forward)
+              ->Field("Back Key", &FirstPersonControllerComponent::m_str_Back)
+              ->Field("Left Key", &FirstPersonControllerComponent::m_str_Left)
+              ->Field("Right Key", &FirstPersonControllerComponent::m_str_Right)
+              ->Field("Camera Yaw Rotate Input", &FirstPersonControllerComponent::m_str_Yaw)
+              ->Field("Camera Pitch Rotate Input", &FirstPersonControllerComponent::m_str_Pitch)
               ->Version(1);
 
             if(AZ::EditContext* ec = sc->GetEditContext())
@@ -17,9 +27,89 @@ namespace FirstPersonController
                     "[First person character cntroller]")
                     ->ClassElement(AZ::Edit::ClassElements::EditorData, "")
                     ->Attribute(AppearsInAddComponentMenu, AZ_CRC_CE("Game"))
-                    ->Attribute(Category, "First Person");
+                    ->Attribute(Category, "First Person")
+                    ->DataElement(nullptr,
+                        &FirstPersonControllerComponent::m_str_Forward,
+                        "Forward Key", "Key for moving forward")
+                    ->DataElement(nullptr,
+                        &FirstPersonControllerComponent::m_str_Back,
+                        "Back Key", "Key for moving back")
+                    ->DataElement(nullptr,
+                        &FirstPersonControllerComponent::m_str_Left,
+                        "Left Key", "Key for moving left")
+                    ->DataElement(nullptr,
+                        &FirstPersonControllerComponent::m_str_Right,
+                        "Right Key", "Key for moving right")
+                    ->DataElement(nullptr,
+                        &FirstPersonControllerComponent::m_str_Pitch,
+                        "Camera Yaw Rotate Input", "Camera yaw rotation control")
+                    ->DataElement(nullptr,
+                        &FirstPersonControllerComponent::m_str_Yaw,
+                        "Camera Pitch Rotate Input", "Camera pitch rotation control");
             }
-                
         }
+
+    }
+
+    void FirstPersonControllerComponent::Activate()
+    {
+        m_MoveForwardEventId = StartingPointInput::InputEventNotificationId(m_str_Forward.c_str());
+        m_MoveBackEventId = StartingPointInput::InputEventNotificationId(m_str_Back.c_str());
+        m_MoveLeftEventId = StartingPointInput::InputEventNotificationId(m_str_Left.c_str());
+        m_MoveRightEventId = StartingPointInput::InputEventNotificationId(m_str_Right.c_str());
+        m_RotateYawEventId = StartingPointInput::InputEventNotificationId(m_str_Yaw.c_str());
+        m_RotatePitchEventId = StartingPointInput::InputEventNotificationId(m_str_Pitch.c_str());
+        InputEventNotificationBus::MultiHandler::BusConnect(m_MoveForwardEventId);
+        InputEventNotificationBus::MultiHandler::BusConnect(m_MoveBackEventId);
+        InputEventNotificationBus::MultiHandler::BusConnect(m_MoveLeftEventId);
+        InputEventNotificationBus::MultiHandler::BusConnect(m_MoveRightEventId);
+        InputEventNotificationBus::MultiHandler::BusConnect(m_RotateYawEventId);
+        InputEventNotificationBus::MultiHandler::BusConnect(m_RotatePitchEventId);
+        AZ::TickBus::Handler::BusConnect();
+    }
+
+    void FirstPersonControllerComponent::Deactivate()
+    {
+        AZ::TickBus::Handler::BusDisconnect();
+        InputEventNotificationBus::MultiHandler::BusDisconnect();
+    }
+
+    void FirstPersonControllerComponent::OnPressed(float value)
+    {
+        const InputEventNotificationId* inputId = InputEventNotificationBus::GetCurrentBusId();
+        if (inputId == nullptr)
+        {
+            return;
+        }
+
+        if(*inputId == m_MoveForwardEventId)
+        {
+            AZ_Printf("", "Forward");
+        }
+        else if(*inputId == m_MoveBackEventId)
+        {
+            AZ_Printf("", "Back");
+        }
+        else if(*inputId == m_MoveLeftEventId)
+        {
+            AZ_Printf("", "Left");
+        }
+        else if(*inputId == m_MoveRightEventId)
+        {
+            AZ_Printf("", "Right");
+        }
+        else if(*inputId == m_RotateYawEventId)
+        {
+            AZ_Printf("", "Yaw");
+        }
+        else if(*inputId == m_RotatePitchEventId)
+        {
+            AZ_Printf("", "Pitch");
+        }
+    }
+
+    void FirstPersonControllerComponent::OnTick(float, AZ::ScriptTimePoint)
+    {
+        ;
     }
 }
--- Code/Source/Clients/FirstPersonControllerComponent.h
+++ Code/Source/Clients/FirstPersonControllerComponent.h
@@ -1,20 +1,53 @@
 #pragma once
 #include <AzCore/Component/Component.h>
+#include <AzCore/Component/TickBus.h>
+#include <AzCore/Math/Vector3.h>
+#include <AzCore/std/string/string.h>
+#include <StartingPointInput/InputEventNotificationBus.h>
 
 namespace FirstPersonController
 {
+    class FirstPersonInput
+    {
+    public:
+        float m_forwardAxis = 0;
+        float m_strafeAxis = 0;
+        float m_viewYaw = 0;
+        float m_viewPitch = 0;
+    };
+
     class FirstPersonControllerComponent
         : public AZ::Component
+        , public AZ::TickBus::Handler
+        , public StartingPointInput::InputEventNotificationBus::MultiHandler
     {
     public:
         AZ_COMPONENT(FirstPersonControllerComponent, "{0a47c7c2-0f94-48dd-8e3f-fd55c30475b9}");
 
         static void Reflect(AZ::ReflectContext* rc);
 
-        void Activate() override {};
-        void Deactivate() override {};
+        // AZ::Component interface implementation
+        void Activate() override;
+        void Deactivate() override;
+
+        // AZ::InputEventNotificationBus interface
+        void OnPressed(float value) override;
+
+        // TickBus interface
+        void OnTick(float deltaTime, AZ::ScriptTimePoint) override;
 
     private:
-        ;
+        StartingPointInput::InputEventNotificationId m_MoveForwardEventId;
+        AZStd::string m_str_Forward;
+        StartingPointInput::InputEventNotificationId m_MoveBackEventId;
+        AZStd::string m_str_Back;
+        StartingPointInput::InputEventNotificationId m_MoveLeftEventId;
+        AZStd::string m_str_Left;
+        StartingPointInput::InputEventNotificationId m_MoveRightEventId;
+        AZStd::string m_str_Right;
+        StartingPointInput::InputEventNotificationId m_RotateYawEventId;
+        AZStd::string m_str_Yaw;
+        StartingPointInput::InputEventNotificationId m_RotatePitchEventId;
+        AZStd::string m_str_Pitch;
     };
 }
