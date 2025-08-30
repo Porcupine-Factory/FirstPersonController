b6d9fbaf88423695843acf78603156955462f91f
Eric Kuzmenko <eric.n.kuzmenko@gmail.com>
2025-08-29 23:58:16 -0400

    Use correct #ifndef, #define, #endif guards
    Signed-off-by: Eric Kuzmenko <eric.n.kuzmenko@gmail.com>

--- Code/Include/FirstPersonController/FirstPersonControllerComponentBus.h
+++ Code/Include/FirstPersonController/FirstPersonControllerComponentBus.h
@@ -2,9 +2,9 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#pragma once
-
+#ifndef FIRST_PERSON_CONTROLLER
 #define FIRST_PERSON_CONTROLLER
+#pragma once
 
 #include <AzCore/Component/ComponentBus.h>
 #include <AzCore/RTTI/BehaviorContext.h>
@@ -532,3 +532,4 @@ namespace FirstPersonController
         }
     };
 } // namespace FirstPersonController
+#endif // FIRST_PERSON_CONTROLLER
