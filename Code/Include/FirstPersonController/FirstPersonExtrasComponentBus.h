2ad7b5ea1dc19f363da4356d2e57121f90649783
NanaYellen <nanayellen@tutanota.com>
2025-11-13 01:03:09 -0500

    Add initial HeadBob functionality to FirstPersonExtrasComponent.

--- Code/Include/FirstPersonController/FirstPersonExtrasComponentBus.h
+++ Code/Include/FirstPersonController/FirstPersonExtrasComponentBus.h
@@ -17,6 +17,9 @@ namespace FirstPersonController
 
         virtual float GetJumpPressedInAirQueueTimeThreshold() const = 0;
         virtual void SetJumpPressedInAirQueueTimeThreshold(const float&) = 0;
+        virtual bool GetEnableHeadBob() const = 0;
+        virtual AZ::EntityId GetHeadBobEntityId() const = 0;
+        virtual void SetHeadBobEntityId(const AZ::EntityId&) = 0;
     };
 
     using FirstPersonExtrasComponentRequestBus = AZ::EBus<FirstPersonExtrasComponentRequests>;
