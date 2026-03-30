ba0f4801eec58710cbcac45d205bcadc5987d623
Eric Kuzmenko <eric.n.kuzmenko@gmail.com>
2026-03-29 21:50:09 -0400

    Modify the cmake files in preperation to use the Multiplayer gem from the O3DE 2605 release which will use PhysX5
    All occurrences of "AMultiplayerPhysX5" will need to be replaced with
    "Multiplayer" once 2605 is released.
    
    Signed-off-by: Eric Kuzmenko <eric.n.kuzmenko@gmail.com>

--- Code/firstpersoncontroller_api_files.cmake
+++ Code/firstpersoncontroller_api_files.cmake
@@ -1,5 +1,35 @@
+# Determine if the Multiplayer gem is enabled
+set(MULTIPLAYER_GEM_ENABLED FALSE)
 
-if(TARGET Gem::AMultiplayerPhysX5.Unified.Static)
+# Check project.json for the Multiplayer gem
+if(NOT MULTIPLAYER_GEM_ENABLED AND EXISTS "${CMAKE_SOURCE_DIR}/project.json")
+    file(READ "${CMAKE_SOURCE_DIR}/project.json" _project_json_content)
+    if(_project_json_content MATCHES "\"AMultiplayerPhysX5\"" OR
+       _project_json_content MATCHES "\"AMultiplayerPhysX5==" OR
+       _project_json_content MATCHES "\"AMultiplayerPhysX5>=" OR
+       _project_json_content MATCHES "\"AMultiplayerPhysX5~=" OR
+       _project_json_content MATCHES "\"AMultiplayerPhysX5>" OR
+       _project_json_content MATCHES "\"AMultiplayerPhysX5<")
+        set(MULTIPLAYER_GEM_ENABLED TRUE)
+    endif()
+endif()
+
+# Fallback to checking enabled_gems.cmake
+if(NOT MULTIPLAYER_GEM_ENABLED AND EXISTS "${CMAKE_SOURCE_DIR}/Code/enabled_gems.cmake")
+    file(READ "${CMAKE_SOURCE_DIR}/Code/enabled_gems.cmake" _enabled_gems_content)
+    if(_enabled_gems_content MATCHES "\"AMultiplayerPhysX5\"" OR _enabled_gems_content MATCHES "[ \t\r\n]AMultiplayerPhysX5[ \t\r\n]")
+        set(MULTIPLAYER_GEM_ENABLED TRUE)
+    endif()
+endif()
+
+# When building the engine workspace directly, standard gems are included
+if(NOT MULTIPLAYER_GEM_ENABLED AND EXISTS "${CMAKE_SOURCE_DIR}/engine.json")
+    if(EXISTS "${CMAKE_SOURCE_DIR}/Gems/AMultiplayerPhysX5")
+        set(MULTIPLAYER_GEM_ENABLED TRUE)
+    endif()
+endif()
+
+if(MULTIPLAYER_GEM_ENABLED)
 add_compile_definitions(NETWORKFPC)
 set(FILES
     Include/FirstPersonController/FirstPersonControllerBus.h
