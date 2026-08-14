34814700495699afefce7a389298d7c8d2026f75
Eric Kuzmenko <eric.n.kuzmenko@gmail.com>
2026-08-14 19:21:55 -0400

    firstpersoncontroller_api_files.cmake: Remove the redundant check seeing if the Multiplayer gem is enabled, move NETWORKFPC compile definition into CMakeLists.txt
    Signed-off-by: Eric Kuzmenko <eric.n.kuzmenko@gmail.com>

--- Code/firstpersoncontroller_api_files.cmake
+++ Code/firstpersoncontroller_api_files.cmake
@@ -1,36 +1,4 @@
-# Determine if the Multiplayer gem is enabled
-set(MULTIPLAYER_GEM_ENABLED FALSE)
-
-# Check project.json for the Multiplayer gem
-if(NOT MULTIPLAYER_GEM_ENABLED AND EXISTS "${CMAKE_SOURCE_DIR}/project.json")
-    file(READ "${CMAKE_SOURCE_DIR}/project.json" _project_json_content)
-    if(_project_json_content MATCHES "\"Multiplayer\"" OR
-       _project_json_content MATCHES "\"Multiplayer==" OR
-       _project_json_content MATCHES "\"Multiplayer>=" OR
-       _project_json_content MATCHES "\"Multiplayer~=" OR
-       _project_json_content MATCHES "\"Multiplayer>" OR
-       _project_json_content MATCHES "\"Multiplayer<")
-        set(MULTIPLAYER_GEM_ENABLED TRUE)
-    endif()
-endif()
-
-# Fallback to checking enabled_gems.cmake
-if(NOT MULTIPLAYER_GEM_ENABLED AND EXISTS "${CMAKE_SOURCE_DIR}/Code/enabled_gems.cmake")
-    file(READ "${CMAKE_SOURCE_DIR}/Code/enabled_gems.cmake" _enabled_gems_content)
-    if(_enabled_gems_content MATCHES "\"Multiplayer\"" OR _enabled_gems_content MATCHES "[ \t\r\n]Multiplayer[ \t\r\n]")
-        set(MULTIPLAYER_GEM_ENABLED TRUE)
-    endif()
-endif()
-
-# When building the engine workspace directly, standard gems are included
-if(NOT MULTIPLAYER_GEM_ENABLED AND EXISTS "${CMAKE_SOURCE_DIR}/engine.json")
-    if(EXISTS "${CMAKE_SOURCE_DIR}/Gems/Multiplayer")
-        set(MULTIPLAYER_GEM_ENABLED TRUE)
-    endif()
-endif()
-
 if(MULTIPLAYER_GEM_ENABLED)
-add_compile_definitions(NETWORKFPC)
 set(FILES
     Include/FirstPersonController/FirstPersonControllerBus.h
     Include/FirstPersonController/FirstPersonControllerTypeIds.h
