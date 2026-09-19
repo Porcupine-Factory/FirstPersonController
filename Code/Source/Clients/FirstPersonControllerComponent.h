83d4dc81b67aec05139a065f1a4c7f80653e3b33
Eric Kuzmenko <eric.n.kuzmenko@gmail.com>
2026-09-18 20:37:24 -0400

    Set the default value for `m_rotationDamp` to 60
    Signed-off-by: Eric Kuzmenko <eric.n.kuzmenko@gmail.com>

--- Code/Source/Clients/FirstPersonControllerComponent.h
+++ Code/Source/Clients/FirstPersonControllerComponent.h
@@ -932,7 +932,7 @@ namespace FirstPersonController
         float m_cameraYaw = 0.f;
         AZ::Quaternion m_newLookRotationDelta = AZ::Quaternion::CreateZero();
         AZ::Vector3 m_newLookRotationDeltaRadians = AZ::Vector3::CreateZero();
-        float m_rotationDamp = 30.f;
+        float m_rotationDamp = 60.f;
         float m_cameraPitchMaxAngle = AZ::Constants::HalfPi;
         float m_cameraPitchMinAngle = -AZ::Constants::HalfPi;
         AZ::TransformInterface* m_cameraRotationTransform = nullptr;
